d7fb738e9af37e0b0dd2a4e8fbeef6a9b6132d8c
Viktor Sharha <vsharha07@gmail.com>
2024-09-04 13:06:28 +0100

    changed keyboard style; started work on script

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const keyboard = document.querySelectorAll("#keyboard button");
 const display = document.querySelectorAll(".lines .letter");
+let wordEls = [];
+
+let cursor = 0;
 
 function onReady() {
 	for (let button of keyboard) {
@@ -7,10 +10,69 @@ function onReady() {
 			processInput(this);
 		});
 	}
+
+	let word = [];
+	for (let i = 0; i < display.length; i++) {
+		if (i % 5 == 0 && i != 0) {
+			wordEls.push(word);
+			word = [];
+		}
+
+		word.push(display[i].innerHTML);
+	}
 }
 
 window.onload = onReady();
 
+function reset() {
+	cursor = 0;
+	for (let letter of display) {
+		letter.innerHTML = "";
+	}
+}
+
+function getStrWords() {
+	let words = [];
+	for (let word of wordEls) {
+		words.push(word.join(""));
+	}
+	return words;
+}
+
+function getCurrentWord() {
+	words = getStrWords();
+
+	let currentWord = "";
+
+	for (let word of words) {
+		if (word.length != 5) {
+			break;
+		}
+		currentWord = word;
+	}
+
+	return currentWord;
+}
+
+function reset() {
+	cursor = 0;
+}
+
 function processInput(button) {
-	console.log(button.innerHTML);
+	switch (button.id) {
+		case "enter":
+			return;
+		case "delete":
+			return;
+	}
+
+	display[cursor].innerHTML = button.innerHTML;
+
+	let word = getCurrentWord();
+	console.log(word);
+
+	cursor++;
+	if (cursor > 29) {
+		reset();
+	}
 }
